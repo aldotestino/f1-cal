af2540b1c97e15f2aa7fa08f9d7f8775f24cf2a7
aldotestino <aldotestino4@gmail.com>
2024-09-21 20:51:26 +0200

    color code status of session

--- src/components/race-card.tsx
+++ src/components/race-card.tsx
@@ -8,11 +8,11 @@ function RaceCard({ name, circuit, sessions }: Race) {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>{circuit.flag} {name}</CardTitle>
+        <CardTitle>{circuit.flag}{' '}{name}</CardTitle>
         <CardDescription>{circuit.name}, {circuit.country}, {circuit.locality}</CardDescription>
       </CardHeader>
       <CardContent>
-        <ol>
+        <ol className='grid gap-4 sm:gap-8 grid-cols-1 sm:grid-cols-2'>
           {sessions.map(s => (
             <SessionItem key={s.type} {...s} />
           ))}
@@ -21,7 +21,7 @@ function RaceCard({ name, circuit, sessions }: Race) {
       <CardFooter className='justify-end'>
         <Button className='space-x-2 w-full sm:w-auto'>
           <CalendarPlus className='w-4 h-4'/>
-          <span>Add to calendar</span>
+          <span>Add all events to calendar</span>
         </Button>
       </CardFooter>
     </Card>
--- src/components/session-item.tsx
+++ src/components/session-item.tsx
@@ -1,17 +1,25 @@
 import { Session } from '@/lib/types';
+import { Button } from './ui/button';
+import { Calendar, CalendarPlus, Timer, Check } from 'lucide-react';
 import { format } from 'date-fns';
+import { cn } from '@/lib/utils';
 
-function SessionItem({ type, start }: Session) {
+function SessionItem({ type, start, status }: Session) {
 
   return (
-    <li className='flex items-center justify-between'>
+    <li className='flex items-center justify-between gap-4'>
       <div className='flex-1 flex items-center gap-4'>
-        <div className='w-10 h-10 rounded-full bg-muted shrink-0' />
+        <div className={cn('w-10 h-10 rounded-full bg-transparent shrink-0 grid place-items-center border-2 border-dashed', status === 'upcoming' ? 'border-blue-500' : status === 'ongoing' ? 'border-yellow-500' : 'border-green-500')}>
+          {status === 'upcoming' ? <Calendar className='w-4 h-4 text-blue-500' /> : status === 'ongoing' ? <Timer className='w-4 h-4 text-yellow-500' /> : <Check className='w-4 h-4 text-green-500' />}
+        </div>
         <div className='flex flex-col'>
           <span className='font-semibold text-lg'>{type}</span>
           <span className='text-muted-foreground text-sm'>{format(start, 'PPPPp')}</span>
         </div>
       </div>
+      {status === 'upcoming' && <Button size="icon" variant="outline">
+        <CalendarPlus className='w-4 h-4' />
+      </Button>}
     </li>
   );
 }
